303580232523c3b598b553e598cc2a3c07ba42b7
elhaidar <elhaidar45@gmail.com>
2023-05-16 14:20:07 +0700

    .

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import ErrorPage from "./pages/404.jsx";
 import Explore from "./pages/ExplorePage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import { Provider } from "./components/context/Context";
-import MovieDetails from "./components/elements/Explore/movieDetails";
 
 const router = createBrowserRouter([
   {
@@ -17,10 +16,6 @@ const router = createBrowserRouter([
   {
     path: "/explore",
     element: <Explore />,
-  },
-  {
-    path: "/movie",
-    element: <MovieDetails />,
   },
 ]);
 
